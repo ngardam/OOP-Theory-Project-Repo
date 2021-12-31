d3c3f11cf37b89b4238b21938216ac6b31e591b4
ngardam <71029615+ngardam@users.noreply.github.com>
2021-12-30 20:19:27 -0500

    Hexsphere height randomizing and color by height
    randomize tile height and smart randomize methods added to HexasphereBuilder.
    
    Color by height method added

--- OOP Theory Project/Assets/Scripts/UI/HexasphereBuilder.cs
+++ OOP Theory Project/Assets/Scripts/UI/HexasphereBuilder.cs
@@ -9,7 +9,22 @@ public class HexasphereBuilder : MonoBehaviour
     [SerializeField] GameObject seedHexasphere;
     [SerializeField] Slider divisionsSlider;
 
+    [SerializeField] float generationSpeed = 0.1f;
+
+    [SerializeField] float neighborHeightWeight = 1f;
+
+    private (Color, float)[] colorHeights = new (Color, float)[]
+    {
+        (Color.white, 0.7f),
+        (Color.gray, 0.61f),
+        (Color.green, 0.5f),
+        (Color.yellow, 0.48f),
+        (Color.blue, 0.0f)
+
+    };
+
     GameObject newHex;
+    Hexasphere hexa;
 
     private void Awake()
     {
@@ -34,7 +49,7 @@ public class HexasphereBuilder : MonoBehaviour
         int divisions = Mathf.FloorToInt(divisionsSlider.value);
         newHex = Instantiate(seedHexasphere);
 
-        Hexasphere hexa = newHex.GetComponent<Hexasphere>();
+        hexa = newHex.GetComponent<Hexasphere>();
         hexa.numDivisions = divisions;
 
         seedHexasphere.SetActive(false);
@@ -44,7 +59,121 @@ public class HexasphereBuilder : MonoBehaviour
 
     public void deleteHexasphere()
     {
-        
+        Destroy(newHex);
     }
+
+    public void RandomizeHeightsStart()
+    {
+        StartCoroutine(RandomizeHeights());
+    }
+
+    public void SmartHeightsStart()
+    {
+        StartCoroutine(SmartHeights());
+    }
+
+    private IEnumerator SmartHeights()
+    {
+        if (hexa != null)
+        {
+            Tile[] arrayOfTiles = hexa.tiles;
+            foreach (Tile tile in arrayOfTiles)
+            {
+                int[] neighborIndexes = hexa.GetTileNeighbours(tile.index);
+                float[] neighborHeights = new float[neighborIndexes.Length];
+                float neighborHeightTotal = 0;
+                int numOfGeneratedNeighbors = 0;
+                float averageNeighborHeight = 0f;
+
+                for (int i = 0; i < neighborIndexes.Length; i++)
+                {
+                    float _height = hexa.GetTileExtrudeAmount(neighborIndexes[i]);
+                    if (_height > 0)
+                    {
+                        //height has been set already
+                        neighborHeightTotal += _height;
+                        numOfGeneratedNeighbors++;
+                        neighborHeights[i] = _height;
+                    }
+                    //neighborHeights[i] = tile.extrudeAmount;
+                    //Debug.Log("neighbor height: " + neigh);
+                    //neighborHeightTotal += neighborHeights[i];
+                }
+
+                if (numOfGeneratedNeighbors > 0)
+                {
+                    averageNeighborHeight = neighborHeightTotal / (numOfGeneratedNeighbors);
+                }
+
+                float randomHeight = Random.Range(0f, 1f);
+                Debug.Log("average neighbor height: " + averageNeighborHeight);
+                Debug.Log("new random height: " + randomHeight);
+                float weightedNewHeight = ((numOfGeneratedNeighbors * (averageNeighborHeight * neighborHeightWeight)) + randomHeight) / ((numOfGeneratedNeighbors * neighborHeightWeight) + 1);
+                Debug.Log("smart height: " + weightedNewHeight);
+                hexa.SetTileExtrudeAmount(tile.index, weightedNewHeight);
+
+                yield return new WaitForSeconds(generationSpeed);
+            }
+        }
+    }
+
+    public IEnumerator RandomizeHeights()
+    {
+       // bool inProgress = true;
+
+
+        if (hexa != null)
+        {
+            Tile[] arrayOfTiles = hexa.tiles;
+            foreach (Tile tile in arrayOfTiles)
+            {
+                float height = Random.Range(0f, 1f);
+                //tile.extrudeAmount = height;
+                hexa.SetTileExtrudeAmount(tile.index, height);
+                Debug.Log("changing Tile height" + height);
+                yield return new WaitForSeconds(generationSpeed);
+            }
+
+            
+        } else
+        {
+           // inProgress = false;
+        }
+
+        
+
+
+    }
+
+    private Color HeightToColor(float height)
+    {
+        Color newColor = Color.magenta;
+        foreach ((Color, float) color in colorHeights)
+        {
+            if (height >= color.Item2)
+            {
+                newColor = color.Item1;
+                break;
+            }
+
+        }
+        return newColor;
+    }
+
+    public void ColorHeights()
+    {
+        Tile[] arrayOfTiles = hexa.tiles;
+
+        foreach ( Tile tile in arrayOfTiles)
+        {
+            float height = hexa.GetTileExtrudeAmount(tile.index);
+            Color newColor = HeightToColor(height);
+            hexa.SetTileColor(tile.index, newColor);
+        }
+    }
+
+    
+
+
 
 }
