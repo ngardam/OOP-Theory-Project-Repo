6fdb1814cbceb992bff08758407f2464e20d899b
ngardam <71029615+ngardam@users.noreply.github.com>
2021-12-30 22:41:08 -0500

    Smooth hexasphere vertices method
    vertex smoothing functional

--- OOP Theory Project/Assets/Scripts/UI/HexasphereBuilder.cs
+++ OOP Theory Project/Assets/Scripts/UI/HexasphereBuilder.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using HexasphereGrid;
+using System;
+
 
 public class HexasphereBuilder : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class HexasphereBuilder : MonoBehaviour
     [SerializeField] float generationSpeed = 0.1f;
 
     [SerializeField] float neighborHeightWeight = 1f;
+
+    [SerializeField] float vertexHeightDamper = 5f;
 
     private (Color, float)[] colorHeights = new (Color, float)[]
     {
@@ -28,13 +32,14 @@ public class HexasphereBuilder : MonoBehaviour
 
     private void Awake()
     {
-
+        
     }
 
     private void Start()
     {
 
-        seedHexasphere.SetActive(true);
+        //seedHexasphere.SetActive(true);
+        buildNewHexasphere();
     }
 
     public void buildNewHexasphere()
@@ -105,7 +110,7 @@ public class HexasphereBuilder : MonoBehaviour
                     averageNeighborHeight = neighborHeightTotal / (numOfGeneratedNeighbors);
                 }
 
-                float randomHeight = Random.Range(0f, 1f);
+                float randomHeight = UnityEngine.Random.Range(0f, 1f);
                 Debug.Log("average neighbor height: " + averageNeighborHeight);
                 Debug.Log("new random height: " + randomHeight);
                 float weightedNewHeight = ((numOfGeneratedNeighbors * (averageNeighborHeight * neighborHeightWeight)) + randomHeight) / ((numOfGeneratedNeighbors * neighborHeightWeight) + 1);
@@ -127,7 +132,7 @@ public class HexasphereBuilder : MonoBehaviour
             Tile[] arrayOfTiles = hexa.tiles;
             foreach (Tile tile in arrayOfTiles)
             {
-                float height = Random.Range(0f, 1f);
+                float height = UnityEngine.Random.Range(0f, 1f);
                 //tile.extrudeAmount = height;
                 hexa.SetTileExtrudeAmount(tile.index, height);
                 Debug.Log("changing Tile height" + height);
@@ -173,7 +178,81 @@ public class HexasphereBuilder : MonoBehaviour
     }
 
     
+    public void VertexTest()
+    {
+        hexa.enableGridEditor = true;
 
+        Vector3[] vertices = hexa.tiles[0].vertices;
+    }
 
+    public void SmoothVertices()
+    {
+        List<Vector3> smoothedVertices;
+
+        Tile[] arrayOfTiles = hexa.tiles;
+
+        int verticesProcessed = 0;
+
+        foreach(Tile tile in arrayOfTiles)
+        {
+            Tile[] neighborTiles = hexa.GetTileNeighboursTiles(tile.index);
+            Vector3[] myVertices = tile.vertices;
+
+            foreach (Vector3 vertex in myVertices)
+            {
+                verticesProcessed++;
+                //  Gizmos.DrawSphere(vertex, 1);
+                //Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere), vertex, Quaternion.identity);
+                Tile[] neighborsSharingVertex = new Tile[2];
+                int neighborsFound = 0;
+
+                    foreach (Tile neighbor in neighborTiles)
+                    {
+                         if (neighborsFound < 2)
+                         {
+                             Vector3[] neighborVertices = neighbor.vertices;
+                             foreach (Vector3 neighborVertex in neighborVertices)
+                             {
+                                 if (neighborVertex == vertex)
+                                 {
+                                     neighborsSharingVertex[neighborsFound] = neighbor;
+                                     neighborsFound++;
+                                     break;
+                                 }
+                             }
+                         }
+                    }
+
+                float averageHeight = AverageTileHeights(tile, neighborsSharingVertex[0], neighborsSharingVertex[1]);
+
+                //float adjustedHeight = averageHeight - hexa.GetTileExtrudeAmount(tile.index);
+
+                hexa.SetTileVertexElevation(tile.index, Array.IndexOf(myVertices, vertex), averageHeight / vertexHeightDamper);
+
+            }
+
+        //   foreach(Tile neighborTile in neighborTiles)
+        //   {
+        //       Vector3[] neighborVertices = neighborTile.vertices;
+        //   }
+        }
+
+        Debug.Log("vertices processed: " + verticesProcessed);
+
+        
+    }
+
+    private float AverageTileHeights(Tile tile1, Tile tile2, Tile tile3)
+    {
+        float tile1Height = hexa.GetTileExtrudeAmount(tile1.index);
+        float tile2Height = hexa.GetTileExtrudeAmount(tile2.index);
+        float tile3Height = hexa.GetTileExtrudeAmount(tile3.index);
+
+        float average = (tile1Height + tile2Height + tile3Height) / 3;
+
+        return average;
+    }
+
+    //void ondra
 
 }
